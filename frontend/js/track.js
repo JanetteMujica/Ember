cdcd0db5ed2101d62d7a67505c275e5c0ce7368b
“Janette <mujica.janette@gmail.com>
2025-03-17 13:13:58 -0400

    react html implementation

---
+++ frontend/js/track.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PainChatbot from '../../src/components/PainChatbot';
+
+ReactDOM.render(
+	<PainChatbot />,
+	document.getElementById('pain-chatbot-container')
+);
---
+++ frontend/js/visualize.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PainVisualization from '../../src/components/PainVisualization';
+
+ReactDOM.render(
+	<PainVisualization />,
+	document.getElementById('pain-visualization-container')
+);
